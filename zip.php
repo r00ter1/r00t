725152db942b106a6b7bdd1fef3f69b0b6f3a2f2
r00t <133220676+r00ter1@users.noreply.github.com>
2024-02-18 04:01:42 +0400

    Update zip.php

--- zip.php
+++ zip.php
@@ -59,23 +59,22 @@ $files = listFiles($currentDir);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>File Manager</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 <body>
     <h1>File Manager</h1>
     <h2>Current Directory: <?php echo $currentDir; ?></h2>
-    <ul id="fileList">
+    <ul>
         <?php foreach($files as $file): ?>
             <li>
                 <?php if(is_dir($currentDir.'/'.$file)): ?>
-                    <a class="dirLink" href="#" data-dir="<?php echo $currentDir.'/'.$file; ?>"><?php echo $file; ?></a>
+                    <a href="?dir=<?php echo $currentDir.'/'.$file; ?>"><?php echo $file; ?></a>
                 <?php else: ?>
                     <?php echo $file; ?>
                 <?php endif; ?>
             </li>
         <?php endforeach; ?>
     </ul>
-    <form id="downloadForm" action="" method="get">
+    <form action="" method="get">
         <input type="hidden" name="action" value="download">
         <input type="hidden" name="dir" value="<?php echo $currentDir; ?>">
         <label for="files">Select files to download:</label><br>
@@ -86,23 +85,5 @@ $files = listFiles($currentDir);
         </select><br>
         <button type="submit">Download Selected Files</button>
     </form>
-    <script>
-        $(document).ready(function(){
-            $('.dirLink').click(function(e){
-                e.preventDefault();
-                var dir = $(this).data('dir');
-                $.get('index.php?dir=' + dir, function(data){
-                    $('#fileList').html(data);
-                    $('#downloadForm input[name="dir"]').val(dir);
-                    $('#downloadForm select[name="files[]"]').html('');
-                    $('#downloadForm button[type="submit"]').prop('disabled', true);
-                });
-            });
-
-            $('#files').change(function(){
-                $('#downloadForm button[type="submit"]').prop('disabled', $(this).val() == null);
-            });
-        });
-    </script>
 </body>
 </html>
